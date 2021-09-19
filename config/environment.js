604ac3dfade5d361dafbc14a289cee3340a26df9
DustySvensson <nessaswanson@hotmail.com>
2021-09-19 21:22:07 +0100

    tiny few tweaks

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,6 @@
-// export const dbURI = 'mongodb://localhost/muggles'
-export const port = 4000
-export const secret = 'this is the secret'
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const dbURI = process.env.DB_URI || 'mongodb+srv://nessa:1ottr6fWUOnE9VVz@cluster0.muz4z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+export const port = process.env.PORT || 4000
+export const secret = process.env.SECRET || 'shhhh its a secret'

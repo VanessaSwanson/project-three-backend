3c153531f01b3b3ab5e06f8fc0ba26c35af76735
DustySvensson <nessaswanson@hotmail.com>
2021-09-21 14:06:10 +0100

    deploy test

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const dbURI = process.env.DB_URI || 'mongodb+srv://nessa:1ottr6fWUOnE9VVz@cluster0.muz4z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+export const dbURI = process.env.DB_URI || 'mongodb+srv://nessa:1ottr6fWUOnE9VVz@cluster0.muz4z.mongodb.net/mugglemore?retryWrites=true&w=majority'
 export const port = process.env.PORT || 4000
 export const secret = process.env.SECRET || 'shhhh its a secret'
